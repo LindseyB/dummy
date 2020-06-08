a3ec25a0cb137f707b81bb8d432cb55cd2d75d33
Lindsey B <hazardous.narayan@gmail.com>
2020-06-08 11:38:24 -0400

    Add some comments

--- make_commits.py
+++ make_commits.py
@@ -1,8 +1,10 @@
 import random
 import os
 
+# zalgoification stolen from a code golf example, could I format this better? yes. will I? no.
 q=lambda z:''.join([v,v+''.join(random.choice(list(map(chr,range(768,815))))for i in range(int(random.normalvariate(10,5))))][v.isalpha()]for v in z)
 
+# these messages taken from a list of most common commit messages
 messages=[
 	"initial commit",
 	"update readme.md",
@@ -22,6 +24,7 @@ messages=[
 	"updates"	
 ]
 
+# I couldn't get the bash for loop to run in the github action, so... here we are.
 for n in range(random.randint(1, 10)):
 	message = q(random.choice(messages))
 	os.system(f'git commit -m "{message}" --allow-empty')
