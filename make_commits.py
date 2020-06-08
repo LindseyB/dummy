ddd44c57be0713c9c4b3574507381ff72a246076
Lindsey B <hazardous.narayan@gmail.com>
2020-06-08 11:14:42 -0400

    ZALGO

--- make_commits.py
+++ make_commits.py
@@ -2,5 +2,5 @@ import random
 import os
 
 for n in range(random.randint(1, 10)):
-	os.system('git commit -m "trash 🗑️🗑️🗑️🗑️🗑️" --allow-empty')
+	os.system('git commit -m "t̤̺̣͓͈̩̝̗́̓̒̇̾̃r̴̝͙͉̣͙̈́́̓̈́͑a̸̧̢̛̬̻̖̩͉̯͆̓̿͌̓̌̎̊͢ͅş̵̥̲̜̳̼̤̫̔̀̎̈́̉͒h̷̨͍̩̗̱͋̊̄̆̅̏̂̎̈́͜͢͠ 🗑️🗑️🗑️🗑️🗑️" --allow-empty')
 	print('did a commit.')
