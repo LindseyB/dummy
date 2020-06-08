f70e8461a0bde81de6e6c6e9adbe3fce40d24c4d
Lindsey B <hazardous.narayan@gmail.com>
2020-06-08 10:50:16 -0400

    Add emoji

--- make_commits.py
+++ make_commits.py
@@ -2,5 +2,5 @@ import random
 import os
 
 for n in range(random.randint(1, 10)):
-	os.system('git commit -m "trash" --allow-empty')
+	os.system('git commit -m "trash 🗑️🗑️🗑️🗑️🗑️" --allow-empty')
 	print('did a commit.')
